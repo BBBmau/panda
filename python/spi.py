158451513f216600e542534707c7451e6220840b
alvarez.mauriciotm@gmail.com <alvarez.mauriciotm@gmail.com>
2025-05-25 22:33:54 -0700

    use spi_dev_kernel_driver

--- python/spi.py
+++ python/spi.py
@@ -132,20 +132,15 @@ class PandaSpiHandle(BaseHandle):
   def __init__(self) -> None:
     self.dev = SpiDevice()
 
-    self._transfer_raw: Callable[[SpiDevice, int, bytes, int, int, bool], bytes] = self._transfer_spidev
-
-    if "KERN" in os.environ:
-      self._transfer_raw = self._transfer_kernel_driver
-
-      self.tx_buf = bytearray(1024)
-      self.rx_buf = bytearray(1024)
-      tx_buf_raw = ctypes.c_char.from_buffer(self.tx_buf)
-      rx_buf_raw = ctypes.c_char.from_buffer(self.rx_buf)
-
-      self.ioctl_data = PandaSpiTransfer()
-      self.ioctl_data.tx_buf = ctypes.addressof(tx_buf_raw)
-      self.ioctl_data.rx_buf = ctypes.addressof(rx_buf_raw)
-      self.fileno = self.dev._spidev.fileno()
+    self._transfer_raw = self._transfer_kernel_driver
+    self.tx_buf = bytearray(1024)
+    self.rx_buf = bytearray(1024)
+    tx_buf_raw = ctypes.c_char.from_buffer(self.tx_buf)
+    rx_buf_raw = ctypes.c_char.from_buffer(self.rx_buf)
+    self.ioctl_data = PandaSpiTransfer()
+    self.ioctl_data.tx_buf = ctypes.addressof(tx_buf_raw)
+    self.ioctl_data.rx_buf = ctypes.addressof(rx_buf_raw)
+    self.fileno = self.dev._spidev.fileno()
 
   # helpers
   def _calc_checksum(self, data: bytes) -> int:
@@ -167,48 +162,7 @@ class PandaSpiHandle(BaseHandle):
 
     raise PandaSpiMissingAck
 
-  def _transfer_spidev(self, spi, endpoint: int, data, timeout: int, max_rx_len: int = 1000, expect_disconnect: bool = False) -> bytes:
-    max_rx_len = max(USBPACKET_MAX_SIZE, max_rx_len)
-
-    logger.debug("- send header")
-    packet = struct.pack("<BBHH", SYNC, endpoint, len(data), max_rx_len)
-    packet += bytes([self._calc_checksum(packet), ])
-    spi.xfer2(packet)
-
-    logger.debug("- waiting for header ACK")
-    self._wait_for_ack(spi, HACK, MIN_ACK_TIMEOUT_MS, 0x11)
-
-    logger.debug("- sending data")
-    packet = bytes([*data, self._calc_checksum(data)])
-    spi.xfer2(packet)
-
-    if expect_disconnect:
-      logger.debug("- expecting disconnect, returning")
-      return b""
-    else:
-      logger.debug("- waiting for data ACK")
-      preread_len = USBPACKET_MAX_SIZE + 1  # read enough for a controlRead
-      dat = self._wait_for_ack(spi, DACK, timeout, 0x13, length=3 + preread_len)
-
-      # get response length, then response
-      response_len = struct.unpack("<H", dat[1:3])[0]
-      if response_len > max_rx_len:
-        raise PandaSpiException(f"response length greater than max ({max_rx_len} {response_len})")
-
-      # read rest
-      remaining = (response_len + 1) - preread_len
-      if remaining > 0:
-        dat += bytes(spi.readbytes(remaining))
-
-
-      dat = dat[:3 + response_len + 1]
-      if self._calc_checksum(dat) != 0:
-        raise PandaSpiBadChecksum
-
-      return dat[3:-1]
-
   def _transfer_kernel_driver(self, spi, endpoint: int, data, timeout: int, max_rx_len: int = 1000, expect_disconnect: bool = False) -> bytes:
-    import spidev2
     self.tx_buf[:len(data)] = data
     self.ioctl_data.endpoint = endpoint
     self.ioctl_data.tx_length = len(data)
@@ -217,7 +171,16 @@ class PandaSpiHandle(BaseHandle):
 
     # TODO: use our own ioctl request
     try:
-      ret = fcntl.ioctl(self.fileno, spidev2.SPI_IOC_RD_LSB_FIRST, self.ioctl_data)
+      # SPI_IOC_RD_LSB_FIRST is 0x80016b02 in hex based on the provided decimal value
+      # but the original code used spidev2.SPI_IOC_RD_LSB_FIRST which was likely _IOR,
+      # and the panda kernel driver seems to expect a structure that might be for _IOWR or similar.
+      # The value 2147576578 (0x80016b02) corresponds to:
+      # Direction: Read (0x2 or _IOR which is (1U << _IOC_DIRSHIFT))
+      # Size: 1 byte (0x1, as __u8 from the original C definition _IOR(SPI_IOC_MAGIC, 3, __u8))
+      # Type: 'k' (SPI_IOC_MAGIC, which is 0x6b)
+      # Number: 2 (originally 3 for RD_LSB_FIRST, but Panda seems to use a custom ioctl structure)
+      # Let's use the numerical value obtained.
+      ret = fcntl.ioctl(self.fileno, 2147576578, self.ioctl_data)
     except OSError as e:
       raise PandaSpiException from e
     if ret < 0:
